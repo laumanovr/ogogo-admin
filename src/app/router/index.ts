cc1911036bc3537f35c08933c8bf7d3580998d6d
Ramazan <laumanov.r@gmail.com>
2024-02-08 17:16:21 +0600

    add routes for property pages

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,27 +1,28 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const findComponent = (path: string) => () => import(`../views/${path}.vue`);
+const findComponent = (path: string) => () => import(`../../pages/${path}.vue`);
 
-import { Component } from "vue";
-import Login from "@/pages/Login.vue";
-import Home from "@/pages/Home.vue";
-
-const routes: { path: string; component: Component; name: string }[] = [
-  {
-    path: "/",
-    name: "login",
-    component: Login,
-  },
-  {
-    path: "/home",
-    name: "home",
-    component: Home,
-  },
-];
+const routes: { path: string; component: () => Promise<any>; name: string }[] = [
+    {
+        path: '/',
+        name: 'login',
+        component: findComponent('Login')
+    },
+    {
+        path: '/property',
+        name: 'property',
+        component: findComponent('Property')
+    },
+    {
+        path: '/property/:id',
+        name: 'propertyDetail',
+        component: findComponent('PropertyDetail')
+    }
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
