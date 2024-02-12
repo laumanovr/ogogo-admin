ff5c75231384ac49703f5531de00d12519638cf1
Ramazan <laumanov.r@gmail.com>
2024-02-12 13:55:08 +0600

    hide header in login-page

--- src/app/layouts/Layout.vue
+++ src/app/layouts/Layout.vue
@@ -1,8 +1,10 @@
 <template>
-  <Header />
+  <Header v-if="route.path !== '/'"/>
   <slot></slot>
 </template>
 
 <script lang="ts" setup>
 import Header from "@/app/layouts/Header.vue";
+import {useRoute} from "vue-router";
+const route = useRoute();
 </script>
