a5df32ee31c73090ac79054c3a79b6d88d45bad2
Ramazan <laumanov.r@gmail.com>
2024-02-13 17:50:20 +0600

    create router for property

---
+++ src/pages/property/router.ts
@@ -0,0 +1,16 @@
+import {findComponent} from "@/shared/lib/utils/findComponent";
+
+const routes = [
+    {
+        path: "/property",
+        name: "property",
+        component: findComponent("property", "Property"),
+    },
+    {
+        path: "/property/:id",
+        name: "propertyDetail",
+        component: findComponent("property", "PropertyDetail"),
+    },
+];
+
+export default routes;
