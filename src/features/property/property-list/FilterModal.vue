cb0a5c5e8cdac199d52237f1c62fada4db7b84f2
Ramazan <laumanov.r@gmail.com>
2024-02-20 14:39:01 +0600

    moved filter-modal to separate file

---
+++ src/features/property/property-list/FilterModal.vue
@@ -0,0 +1,153 @@
+<template>
+  <SModal :isModalOpen="isShowModal" class="filter-modal" width="420px" height="100%"
+          @onClose="toggleFilterModal">
+    <div class="modal-content">
+      <div class="filter-title">Фильтры</div>
+      <div class="section">
+        <div class="section-title">Тип свойства</div>
+        <div>
+          <SRadioButton>Все</SRadioButton>
+        </div>
+        <div>
+          <SRadioButton>Справочник</SRadioButton>
+        </div>
+        <div>
+          <SRadioButton>Ручное</SRadioButton>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section-title">Тип данных</div>
+        <div>
+          <SCheckbox>Текстовой</SCheckbox>
+        </div>
+        <div>
+          <SCheckbox>Числовое</SCheckbox>
+        </div>
+        <div>
+          <SCheckbox>Дробные числа</SCheckbox>
+        </div>
+        <div>
+          <SCheckbox>Булеан</SCheckbox>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section-title">Локализация значении</div>
+        <div>
+          <SRadioButton>Все</SRadioButton>
+        </div>
+        <div>
+          <SRadioButton>Нет</SRadioButton>
+        </div>
+        <div>
+          <SRadioButton>Есть</SRadioButton>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section-title">Группа свойств</div>
+        <SInput isSearchable/>
+        <div class="property-items">
+          <div v-for="item in 20">
+            <SCheckbox>Характеристики</SCheckbox>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="filter-actions">
+      <SButton size="large" color="gray">Сбросить</SButton>
+      <SButton size="large" color="violet">Применить</SButton>
+    </div>
+  </SModal>
+</template>
+
+<script lang="ts" setup>
+import { SModal, SRadioButton, SCheckbox, SInput, SButton } from "@tumarsoft/ogogo-ui";
+import { ref } from "vue";
+
+const isShowModal = ref(false);
+
+const toggleFilterModal = () => {
+  isShowModal.value = !isShowModal.value;
+};
+
+defineExpose({
+  toggleFilterModal
+});
+</script>
+
+<style lang="scss">
+@import "../../../app/style/colors.scss";
+
+.filter-modal {
+  .modal-content {
+    height: calc(100% - 75px);
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: $gray-300;
+      border-radius: 4px;
+    }
+  }
+
+  .filter-title {
+    font-size: 24px;
+    font-weight: 700;
+    margin-bottom: 35px;
+  }
+
+  .section {
+    &:not(:last-child) {
+      margin-bottom: 30px;
+    }
+
+    &-title {
+      font-size: 16px;
+      font-weight: 700;
+      margin-bottom: 20px;
+    }
+  }
+
+  .radio-text, .checkbox-title {
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  .checkbox-container {
+    margin-bottom: 8px;
+  }
+
+  .property-items {
+    overflow-y: auto;
+    max-height: 200px;
+    margin-top: 15px;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: $gray-300;
+      border-radius: 4px;
+    }
+  }
+
+  .filter-actions {
+    display: flex;
+    justify-content: space-between;
+    padding: 24px 0 24px;
+    position: absolute;
+    bottom: 0;
+    width: calc(100% - 56px);
+
+    .button {
+      width: calc(50% - 4px);
+    }
+  }
+}
+</style>
