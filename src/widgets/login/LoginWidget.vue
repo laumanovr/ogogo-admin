1f8866bdfc070a8ec95c833da9ecc77873d5ae59
Ramazan <laumanov.r@gmail.com>
2024-02-21 15:44:26 +0600

    modified login component

--- src/widgets/login/LoginWidget.vue
+++ src/widgets/login/LoginWidget.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="login-container">
-    <Loader v-if="isLoading" />
     <div class="login-block">
       <div class="login-logo">
-        <img src="../../shared/ui/assets/Ogogo-logo.png" alt="img" />
+        <img src="../../shared/ui/assets/Ogogo-logo.png" alt="img"/>
       </div>
       <SForm class="form-block" ref="loginForm">
         <div class="form-title">
@@ -11,24 +10,24 @@
         </div>
         <div>
           <SInput
-            :label="$t('lang-5b5360db-a6ff-43a7-a8d4-f35517b9c4a8')"
-            width="100%"
-            :rules="requiredField"
-            v-model="loginObj.pin"
+              :label="$t('lang-5b5360db-a6ff-43a7-a8d4-f35517b9c4a8')"
+              width="100%"
+              :rules="requiredField"
+              v-model="loginObj.pin"
           />
         </div>
         <div class="input-password">
           <SInput
-            type="password"
-            :label="$t('lang-ad3a8ec6-bcb6-4dce-9ff6-a3ccc17c1e8d')"
-            width="100%"
-            :rules="requiredField"
-            v-model="loginObj.password"
+              type="password"
+              :label="$t('lang-ad3a8ec6-bcb6-4dce-9ff6-a3ccc17c1e8d')"
+              width="100%"
+              :rules="requiredField"
+              v-model="loginObj.password"
           />
         </div>
-        <SButton size="large" color="violet" @click="onSubmitLogin">{{
-          $t("lang-09888a35-0a9f-46d7-bc1e-34bbae78ccd9")
-        }}</SButton>
+        <SButton size="large" color="violet" @click="onSubmitLogin">
+          {{ $t("lang-09888a35-0a9f-46d7-bc1e-34bbae78ccd9") }}
+        </SButton>
       </SForm>
     </div>
   </div>
@@ -40,28 +39,27 @@ import { ref, reactive } from "vue";
 import { useRouter } from "vue-router";
 import { requiredField } from "@/shared/lib/utils/rules";
 import { useAuthStore } from "@/shared/store/auth";
+import { useAlertStore } from "@/shared/store/alert";
+import { useLoaderStore } from "@/shared/store/loader";
+
 const authStore = useAuthStore();
+const alertStore = useAlertStore();
+const loaderStore = useLoaderStore();
 const router = useRouter();
 
-const loginObj = reactive({ pin: "", password: "" });
+const loginObj = reactive({pin: "", password: ""});
 const loginForm = ref(null);
-const isLoading = ref(false);
 
 const onSubmitLogin = () => {
-  const isValid = loginForm.value.validateForm();
-  if (isValid) {
-    isLoading.value = true;
-    authStore
-      .login(loginObj)
-      .then((resp: any) => {
-        console.log(resp);
-        isLoading.value = false;
-        router.push("/property");
-      })
-      .catch((err: any) => {
-        console.log(err);
-        isLoading.value = false;
-      });
+  if (loginForm.value.validateForm()) {
+    loaderStore.setLoaderState(true);
+    authStore.login(loginObj).then(() => {
+      loaderStore.setLoaderState(false);
+      router.push("/property");
+    }).catch((err: any) => {
+      loaderStore.setLoaderState(false);
+      alertStore.showError(err?.error?.errorMessage);
+    });
   }
 };
 </script>
@@ -86,14 +84,17 @@ const onSubmitLogin = () => {
     padding: 24px;
     border-radius: 16px;
     box-shadow: 0 12px 32px 0 rgba(40, 43, 48, 0.15);
+
     .form-title {
       font-weight: 700;
       font-size: 20px;
       margin-bottom: 35px;
     }
+
     .input-password {
       margin: 16px 0 35px;
     }
+
     .button {
       width: 100%;
     }
