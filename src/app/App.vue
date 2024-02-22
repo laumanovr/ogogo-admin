2eed2f20fd1a577486eb917f95ff07369869338e
Emir Azimov <emir.azimov@gmail.com>
2024-02-22 10:47:51 +0600

    fix: merge conflicts

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,8 +1,5 @@
 <template>
   <component :is="currentComponent">
-    <router-view />
-  </component>
-  <component :is="currentComponent">
     <Loader v-if="loaderStore.isLoading" />
     <SToaster ref="toaster" />
     <router-view />
@@ -10,10 +7,13 @@
 </template>
 
 <script lang="ts" setup>
-import "@/app/style/colors.scss";
 import Layout from "@/shared/ui/layouts/Layout.vue";
 import Empty from "@/shared/ui/layouts/Empty.vue";
-import { computed } from "vue";
+import Loader from "@/entities/Loader.vue";
+import { computed, ref, watch } from "vue";
+import { SToaster } from "@tumarsoft/ogogo-ui";
+import { useAlertStore } from "@/shared/store/alert";
+import { useLoaderStore } from "@/shared/store/loader";
 
 const alertStore = useAlertStore();
 const loaderStore = useLoaderStore();
@@ -27,4 +27,31 @@ const currentComponent = computed(() => {
     return Empty;
   }
 });
+
+watch(
+  () => alertStore.successMessage,
+  (newValue: any) => {
+    if (newValue) {
+      toaster.value.showSuccess(newValue);
+    }
+  }
+);
+
+watch(
+  () => alertStore.errorMessage,
+  (newValue: any) => {
+    if (newValue) {
+      toaster.value.showError(newValue);
+    }
+  }
+);
+
+watch(
+  () => alertStore.infoMessage,
+  (newValue: any) => {
+    if (newValue) {
+      toaster.value.showInfo(newValue);
+    }
+  }
+);
 </script>
