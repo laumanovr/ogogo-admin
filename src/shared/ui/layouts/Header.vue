e7f2a81fe3b3cda19f8686acc4fa5ab1f64e68c1
Ramazan <laumanov.r@gmail.com>
2024-02-22 13:47:36 +0600

    modified header active icons

--- src/shared/ui/layouts/Header.vue
+++ src/shared/ui/layouts/Header.vue
@@ -7,8 +7,8 @@
         @click="onMenuItemChange(MenuItems.Products)"
         class="cursor-pointer d-flex flex-col items-center"
       >
-        <SIconRender :color="isProductsActive" :name="'BoxIcon'" />
-        <p :class="`text-${isProductsActive} font-size-12`">
+        <SIconRender :color="isProductsActive.substr(6)" :name="'BoxIcon'" />
+        <p :class="`${isProductsActive} font-size-12`">
           {{ $t("lang-9839245b-e40e-4ae1-92e9-0421dc97a154") }}
         </p>
       </div>
@@ -16,8 +16,8 @@
         @click="onMenuItemChange(MenuItems.Stores)"
         class="cursor-pointer d-flex flex-col items-center"
       >
-        <SIconRender :color="isStoresActive" :name="'ShopIcon'" />
-        <p :class="`text-${isStoresActive} font-size-12`">
+        <SIconRender :color="isStoresActive.substr(6)" :name="'ShopIcon'" />
+        <p :class="`${isStoresActive} font-size-12`">
           {{ $t("lang-425a5a16-bf0a-4ded-a7bc-cbe7490b659a") }}
         </p>
       </div>
@@ -25,9 +25,9 @@
         @click="onMenuItemChange(MenuItems.Category)"
         class="cursor-pointer d-flex flex-col items-center"
       >
-        <SIconRender :color="isCategoryActive" :name="'GridIcon'" />
+        <SIconRender :color="isCategoryActive.substr(6)" :name="'GridIcon'" />
         <!-- <img src="../../ui/assets/Grid.png" /> -->
-        <p :class="`text-${isCategoryActive} font-size-12`">
+        <p :class="`${isCategoryActive} font-size-12`">
           {{ $t("lang-75805fdb-eac2-4c87-b481-563e318789b5") }}
         </p>
       </div>
@@ -35,9 +35,8 @@
           @click="onMenuItemChange(MenuItems.Properties)"
           class="cursor-pointer d-flex flex-col items-center"
       >
-        <SIconRender :color="isPropertyActive" :name="'PropertyIcon'" />
-        <!-- <img src="../../ui/assets/Grid.png" /> -->
-        <p :class="`text-${isPropertyActive} text-size-12`">
+        <SIconRender :color="isPropertyActive.substr(6)" :name="'PropertyIcon'" />
+        <p :class="`${isPropertyActive} font-size-12`" >
           {{ $t("lang-c9b8a310-7c1a-4936-9912-fc00c4d165d2") }}
         </p>
       </div>
@@ -63,16 +62,16 @@ Object.values(MenuItems).forEach((item: any) => {
 });
 
 const isProductsActive = computed(() => {
-  return activeButton.value === MenuItems.Products ? "violet-700" : "gray-400";
+  return activeButton.value === MenuItems.Products ? "color-violet-700" : "gray-400";
 });
 const isStoresActive = computed(() => {
-  return activeButton.value === MenuItems.Stores ? "violet-700" : "gray-400";
+  return activeButton.value === MenuItems.Stores ? "color-violet-700" : "gray-400";
 });
 const isCategoryActive = computed(() => {
-  return activeButton.value === MenuItems.Category ? "violet-700" : "gray-400";
+  return activeButton.value === MenuItems.Category ? "color-violet-700" : "gray-400";
 });
 const isPropertyActive = computed(() => {
-  return activeButton.value === MenuItems.Properties ? "violet-700" : "gray-400";
+  return activeButton.value === MenuItems.Properties ? "color-violet-700" : "gray-400";
 });
 
 function onMenuItemChange(value: MenuItems) {
