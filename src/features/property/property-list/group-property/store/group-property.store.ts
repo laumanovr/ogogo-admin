fbaddbd6968b7145efd9b5ae63037ffe7e842fea
Ramazan <laumanov.r@gmail.com>
2024-02-28 10:28:26 +0600

    add loader and alerts in group-property

--- src/features/property/property-list/group-property/store/group-property.store.ts
+++ src/features/property/property-list/group-property/store/group-property.store.ts
@@ -1,6 +1,11 @@
 import { defineStore } from "pinia";
 import { IGroupProperty } from "./group-property.types";
 import { createGroupProperties, getGroupProperties, updateGroupProperties } from "../api/index.ts";
+import { useLoaderStore } from "@/shared/store/loader";
+import { useAlertStore } from "@/shared/store/alert";
+
+const loaderStore = useLoaderStore();
+const alertStore = useAlertStore();
 
 export const useGroupPropertyStore = defineStore("group-property-store", {
     state: (): IGroupProperty => <IGroupProperty>({
@@ -14,30 +19,41 @@ export const useGroupPropertyStore = defineStore("group-property-store", {
     actions: {
         async fetchGroupPropertyList(payload: any) {
             try {
+                loaderStore.setLoaderState(true);
                 const response = await getGroupProperties(payload);
                 this.groupPropertyList = response?.items;
+                loaderStore.setLoaderState(false);
             } catch (err) {
-                console.log(err);
+                alertStore.showError(err.message);
+                loaderStore.setLoaderState(false);
             }
         },
 
         async createGroupProperty(payload: IGroupProperty) {
             try {
+                loaderStore.setLoaderState(true);
                 const response = await createGroupProperties(payload);
                 const items = response.map((item) => item.result);
-                const currentItems = this.groupPropertyList.reverse()
+                const currentItems = this.groupPropertyList.reverse();
                 this.groupPropertyList = [...currentItems, ...items];
+                loaderStore.setLoaderState(false);
+                alertStore.showSuccess("Успешно добавлено!");
             } catch (err) {
-                console.log(err);
+                alertStore.showError(err.message);
+                loaderStore.setLoaderState(false);
             }
         },
 
         async updateGroupProperty(payload: IGroupProperty) {
             try {
+                loaderStore.setLoaderState(true);
                 await updateGroupProperties(payload);
-                await this.fetchGroupPropertyList({pageSize: 100})
+                await this.fetchGroupPropertyList({pageSize: 100});
+                loaderStore.setLoaderState(false);
+                alertStore.showSuccess("Успешно обновлено!");
             } catch (err) {
-                console.log(err);
+                alertStore.showError(err.message);
+                loaderStore.setLoaderState(false);
             }
         }
     }
