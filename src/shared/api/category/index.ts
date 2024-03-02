e74e5516a6e30fddb7cb1dcedc0f72218892b0d9
Maxim <bboy.mars.97@gmail.com>
2024-03-02 12:09:15 +0600

    feat: add categoryapi class

--- src/shared/api/category/index.ts
+++ src/shared/api/category/index.ts
@@ -1,7 +1,9 @@
 import { API } from "@/shared/lib/plugins/axios";
 import { ICategorySettings } from "./index.types";
 
-export const getCategoryById = (id: string): Promise<ICategorySettings> =>
+export class CategoryApi {
+  getCategoryById = (id: string): Promise<ICategorySettings> =>
   API.get(`Marketplace/GetMarketplaceCategory`, {
     params: { id },
   }).then((res) => res.data.result);
+}
