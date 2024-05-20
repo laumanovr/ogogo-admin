19520b2b23ee624c8264ed772b89e1ddd17685ff
Ramazan <laumanov.r@gmail.com>
2024-05-20 18:47:45 +0600

    product-entity-type

---
+++ src/entities/product/model/types.ts
@@ -0,0 +1,14 @@
+export interface ProductEntity {
+  id: string;
+  productName: string;
+  productType: number;
+  status: number;
+  iconBase64: string;
+  verifierDate: string;
+  verifierId: number;
+  verifierName: string;
+  shopId: string;
+  shopName: string;
+  shopIconBase64: string;
+  changes: string;
+}
