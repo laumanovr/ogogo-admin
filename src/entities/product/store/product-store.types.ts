9bae5cb2ae9adb6bc487a6b753651aed1e80546f
Ramazan <laumanov.r@gmail.com>
2024-05-23 18:01:51 +0600

    add verification-object to send

--- src/entities/product/store/product-store.types.ts
+++ src/entities/product/store/product-store.types.ts
@@ -4,4 +4,30 @@ export interface IProductState {
   moderationProducts: ProductEntity[];
   totalCount: number;
   selectedProduct: ProductDetailEntity;
+  verificationData: {
+    id?: string;
+    verifierComment?: string;
+    validationDetails?: {
+      fields: {
+        productName: {
+          validationComment: string;
+          verified: false;
+        };
+        description: {
+          validationComment: "string";
+          verified: false;
+        };
+        price: {
+          validationComment: "string";
+          verified: false;
+        };
+        countOfProduct: {
+          validationComment: "string";
+          verified: false;
+        };
+      };
+      files: object;
+      properties: object;
+    };
+  };
 }
