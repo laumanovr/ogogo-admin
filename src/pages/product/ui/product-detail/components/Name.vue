2595f06acb13a3fc64730bc99c482f9405cb5a02
Ramazan <laumanov.r@gmail.com>
2024-05-24 17:54:15 +0600

    modified func of add-comment in name-component

--- src/pages/product/ui/product-detail/components/Name.vue
+++ src/pages/product/ui/product-detail/components/Name.vue
@@ -1,13 +1,19 @@
 <template>
-  <div
-    class="comment-block d-flex w-p-100"
-    :class="{
-      rejected: false,
-    }"
-  >
-    <img src="/icons/ava.png" alt="" class="mr-12 comment-icon w-40 h-40" />
+  <div class="comment-block d-flex w-p-100" :class="{ 'bg-gray': hasComment }">
+    <img
+      src="/icons/ogogo-ava.svg"
+      alt=""
+      class="mr-12 comment-icon w-40 h-40"
+    />
+    <CommentContainer
+      v-if="hasComment"
+      :name="'Админ'"
+      :date="product.validationDetails.fields.name.moderationDate"
+      :comment="nameComment"
+      @edit="editNameComment"
+    />
     <CommentInput
-      v-if="true"
+      v-else
       :placeholder="$t('lang-5b31da57-d71a-4a20-9490-6ffd5285671c')"
       :comment="nameComment"
       :is-show-actions="isShowButtons"
@@ -15,25 +21,25 @@
       @submit="addNameComment"
       @cancel="cancelNameComment"
     />
-    <CommentContainer
-      v-else
-      :name="'moder-name'"
-      :date="'01-01-2024'"
-      :comment="nameComment"
-      @edit="editNameComment"
-      @delete="deleteNameComment"
-    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { CommentInput, CommentContainer } from "@/shared/ui";
 import { useProductStore } from "@/entities/product/store/product.store";
 
 const productStore = useProductStore();
 const nameComment = ref("");
 const isShowButtons = ref(false);
+const hasComment = ref(false);
+const product = computed(() => productStore.getSelectedProduct);
+
+onMounted(() => {
+  nameComment.value =
+    product.value.validationDetails.fields.name.validationComment;
+  hasComment.value = Boolean(nameComment.value);
+});
 
 const handleInput = (value: string) => {
   nameComment.value = value;
@@ -41,8 +47,12 @@ const handleInput = (value: string) => {
 };
 
 const addNameComment = () => {
-  console.log(nameComment.value);
-  productStore.saveValidationCommentForField("productName", nameComment.value);
+  productStore.setValidationCommentForField(
+    "name",
+    nameComment.value,
+    product.value.id
+  );
+  hasComment.value = true;
 };
 
 const cancelNameComment = () => {
@@ -50,7 +60,18 @@ const cancelNameComment = () => {
   isShowButtons.value = false;
 };
 
-const editNameComment = () => {};
-
-const deleteNameComment = () => {};
+const editNameComment = () => {
+  hasComment.value = false;
+};
 </script>
+
+<style lang="scss">
+@import "../../../../../app/styles/colors.scss";
+.comment-block {
+  &.bg-gray {
+    background: $gray-50;
+    border-radius: 8px;
+    padding: 5px;
+  }
+}
+</style>
