e7e4d65b3ea9880cd486eebf724346a0fd5431a1
Ramazan <laumanov.r@gmail.com>
2024-05-31 11:08:30 +0600

    add type to selected-property

--- src/entities/property-value/store/types.ts
+++ src/entities/property-value/store/types.ts
@@ -1,6 +1,7 @@
 import { PropertyValueEntity } from "@/entities/property-value";
+import { PropertyEntity } from "@/entities/property/model/PropertyEntity";
 
 export interface PropertyValueState {
   propertyValueList: PropertyValueEntity[];
-  selectedProperty: any; // TODO: replace this type
+  selectedProperty: PropertyEntity;
 }
