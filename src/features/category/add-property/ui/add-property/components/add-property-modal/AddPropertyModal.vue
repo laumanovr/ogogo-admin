b6728db52aa2b2cccefcfd819f4da144e37f0485
Ramazan <laumanov.r@gmail.com>
2024-06-10 15:44:52 +0600

    add-property-modal modified

--- src/features/category/add-property/ui/add-property/components/add-property-modal/AddPropertyModal.vue
+++ src/features/category/add-property/ui/add-property/components/add-property-modal/AddPropertyModal.vue
@@ -1,10 +1,5 @@
 <template>
-  <SModal
-    :isModalOpen="props.value"
-    height="auto"
-    @onClose="onClose"
-    :width="'440px'"
-  >
+  <SModal v-model="isModalOpen" height="auto" :width="'440px'">
     <div class="add-property-modal-main-wrapper">
       <p class="font-bold">
         {{ $t("lang-13910017-20f7-43fd-a9a8-7dbcd2b474f0") }}
@@ -13,8 +8,8 @@
         v-model="propertyObject.propertyId"
         :label="$t('lang-c9b8a310-7c1a-4936-9912-fc00c4d165d2')"
         class="w-p-100"
-        showValue="value"
-        getValue="id"
+        itemTitle="value"
+        itemValue="id"
         :items="getPropertiesListAutocomplete"
       />
       <SCheckbox id="0" class="w-p-100">
@@ -25,8 +20,8 @@
         :label="$t('lang-d5ed16f8-b6af-4c0a-ba0a-820cf3ce91ba')"
         class="w-p-100"
         :items="renderPropertyTypes"
-        showValue="text"
-        getValue="value"
+        itemTitle="text"
+        itemValue="value"
       />
       <SSwitch
         labelPosition="left"
@@ -46,11 +41,11 @@
         v-model="propertyObject.isAddNameToProductName"
         >{{ $t("lang-4ae87450-36ee-4e92-a2a1-071898911d00") }}</SSwitch
       >
-      <div class="buttons-container">
-        <SButton color="gray" class="button" @click="onClose">
+      <div class="buttons-container light">
+        <SButton type="secondary" class="button" @click="onClose">
           {{ $t("lang-c66fcd83-27ea-4c39-a1e9-be9c01dfdb36") }}</SButton
         >
-        <SButton color="violet" class="button" @click="onSave">
+        <SButton class="button" @click="onSave">
           {{ $t("lang-d0108c3e-9945-4245-a730-b011e5a47c35") }}</SButton
         >
       </div>
@@ -59,7 +54,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive, onBeforeMount } from "vue";
+import { computed, ref, reactive, onBeforeMount } from "vue";
 import {
   SButton,
   SModal,
@@ -74,12 +69,7 @@ import i18n from "@/shared/lib/plugins/i18n";
 import { useAddPropertyStore } from "@/features/category/add-property/store/add-property.store";
 import { useCategoryStore } from "@/entities/category";
 
-const props = defineProps({
-  value: {
-    type: Boolean,
-    default: false,
-  },
-});
+const isModalOpen = ref(false);
 const emit = defineEmits(["close"]);
 
 onBeforeMount(() => {
@@ -115,8 +105,12 @@ const renderPropertyTypes = reactive([
   },
 ]);
 
+const togglePropertyModal = () => {
+  isModalOpen.value = !isModalOpen.value;
+};
+
 const onClose = () => {
-  emit("close", false);
+  isModalOpen.value = false;
 };
 
 const onSave = () => {
@@ -126,6 +120,10 @@ const onSave = () => {
   categoryStore.setProperties({ ...propertyObject });
   onClose();
 };
+
+defineExpose({
+  togglePropertyModal,
+});
 </script>
 
 <style scoped lang="scss">
