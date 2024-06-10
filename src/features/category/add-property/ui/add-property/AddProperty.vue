7119058a33bc9ae43e8e004999afc50e0c61a324
Ramazan <laumanov.r@gmail.com>
2024-06-10 15:42:36 +0600

    add-property modified

--- src/features/category/add-property/ui/add-property/AddProperty.vue
+++ src/features/category/add-property/ui/add-property/AddProperty.vue
@@ -1,33 +1,30 @@
 <template>
-  <SButton color="gray" class="add-button" @click="onOpenModal">
-    <img src="/icons/plus-icon.png" />
-    <p>{{ $t("lang-af8b01aa-014b-421c-98fd-e68365f64cf4") }}</p></SButton
-  >
+  <div class="light">
+    <SButton type="secondary" class="add-button" @click="onOpenModal">
+      <img src="/icons/plus-icon.png" />
+      <p>{{ $t("lang-1d55fd08-eaa6-4b4b-84b9-225f2d0dc1ad") }}</p></SButton
+    >
+  </div>
   <PropertyCardList
     v-for="(propertyItem, i) in categoryStore.getProperties"
     :key="i"
     :property="propertyItem"
   />
-  <AddPropertyModal :value="modalValue" @close="onClose" />
+  <AddPropertyModal ref="propertyModal" />
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import { SButton } from "@tumarsoft/ogogo-ui";
 import { AddPropertyModal } from "@/features/category/add-property";
-import { ref } from "vue";
-
 import { useCategoryStore } from "@/entities/category";
 import { PropertyCardList } from "@/widgets/category-settings/ui/components";
-let modalValue = ref(false);
 
 const categoryStore = useCategoryStore();
+const propertyModal = ref(null);
 
 const onOpenModal = () => {
-  modalValue.value = true;
-};
-
-const onClose = () => {
-  modalValue.value = false;
+  propertyModal.value.togglePropertyModal();
 };
 </script>
 
