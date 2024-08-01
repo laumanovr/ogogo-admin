ef3928e9bb1bb06220a8ecc78fe124c05e8fa4d2
Ramazan <laumanov.r@gmail.com>
2024-08-01 15:06:06 +0600

    modified app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,67 +1,29 @@
 <template>
   <component :is="currentComponent">
-    <Loader v-if="loaderStore.isLoading" />
-    <SToaster ref="toaster" position="top-center" />
+    <SAlert :items="alertItems" @close="closeAlert" />
     <router-view />
   </component>
 </template>
 
 <script lang="ts" setup>
 import { Layout, Empty } from "@/shared/layouts";
-import { Loader } from "@/shared/ui";
-import { ref, watch, computed } from "vue";
-import { SToaster } from "@tumarsoft/ogogo-ui";
+import { computed } from "vue";
+import { SAlert } from "@tumarsoft/ogogo-ui";
 import { useAlertStore } from "@/shared/store/alert";
-import { useLoaderStore } from "@/shared/store/loader";
-import { useRoute } from "vue-router";
-import { ROUTES } from "@/shared/router/index.type";
 import { useAuthStore } from "@/shared/store/auth";
-import { Nullable } from "@/shared/lib/utils/nullable";
-
-type Toaster = {
-  showSuccess: (message: string) => void;
-  showError: (message: string) => void;
-  showInfo: (message: string) => void;
-};
 
 const alertStore = useAlertStore();
-const loaderStore = useLoaderStore();
 const authStore = useAuthStore();
-const route = useRoute();
-const toaster = ref<Nullable<Toaster>>(null);
+const alertItems = computed(() => alertStore.getAlertItems);
+
+const closeAlert = (id: string) => {
+  alertStore.closeAlert(id);
+};
 
 const currentComponent = computed(() => {
-  if (route.path !== ROUTES.login && Boolean(authStore.getSessionId)) {
+  if (Boolean(authStore.getSessionId)) {
     return Layout;
-  } else {
-    return Empty;
-  }
+  } 
+  return Empty;
 });
-
-watch(
-  () => alertStore.successMessage,
-  (newValue: any) => {
-    if (newValue) {
-      toaster.value.showSuccess(newValue);
-    }
-  }
-);
-
-watch(
-  () => alertStore.errorMessage,
-  (newValue: any) => {
-    if (newValue) {
-      toaster.value.showError(newValue);
-    }
-  }
-);
-
-watch(
-  () => alertStore.infoMessage,
-  (newValue: any) => {
-    if (newValue) {
-      toaster.value.showInfo(newValue);
-    }
-  }
-);
 </script>
