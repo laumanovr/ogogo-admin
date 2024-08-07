8a65e32a66c4a7fedbb20d14f095ae2d0abe94bb
Ramazan <laumanov.r@gmail.com>
2024-08-07 17:52:31 +0600

    add composable

--- src/shared/composable/index.ts
+++ src/shared/composable/index.ts
@@ -4,3 +4,7 @@ export const getFileById = (fileId: string) => {
   const authStore = useAuthStore();
   return `${import.meta.env.VITE_API_SERVER}File/FileById?id=${fileId}&sessionId=${authStore.getSessionId}`;
 };
+
+export const getFullIcon = (iconPath: string) => {
+  return `data:image/png;base64,${iconPath}`
+}
