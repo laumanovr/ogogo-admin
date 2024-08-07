7f9eb33f446a8d15e72c7f3facc4463c8f6c809c
Ramazan <laumanov.r@gmail.com>
2024-08-07 17:52:51 +0600

    add enum

--- src/shared/lib/utils/enums.ts
+++ src/shared/lib/utils/enums.ts
@@ -14,3 +14,15 @@ export enum Mode {
   create = "create",
   update = "update",
 }
+
+export enum ProductStatus {
+  ALL = 0,
+  DRAFT = 14800,
+  PUBLISHED = 14801,
+  AWAITING_MODERATION = 14802,
+  AWAITING_APPROVE_TO = 14803,
+  APPROVED = 14804,
+  ARCHIVED = 14805,
+  REQUIRE_IMPROVEMENT = 14806,
+  BLOCKED = 14807,
+}
