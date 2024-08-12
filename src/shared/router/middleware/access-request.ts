fbfcb9d3e89ce3d88ea5a5e1d184f8d4e25c07c2
Ramazan <laumanov.r@gmail.com>
2024-08-12 17:52:14 +0600

    modified file

--- src/shared/router/middleware/access-request.ts
+++ src/shared/router/middleware/access-request.ts
@@ -3,15 +3,3 @@ import { BaseHandler } from "@/shared/router/middleware/auth.type";
 export class AccessRequestMiddleware extends BaseHandler {
   handle(): void {}
 }
-
-export enum AlertPositionEnum {
-  "TopLeft" = "TopLeft",
-  "TopRight" = "TopRight",
-  "BottomLeft" = "BottomLeft",
-  "BottomRight" = "BottomRight",
-  "Top" = "Top",
-  "Bottom" = "Bottom",
-  "Left" = "Left",
-  "Right" = "Right",
-  "Center" = "Center",
-}
